df043859885693cde36ff41f062aad43d9e95ee5
Zyb Jared Valdez <zyb.valdez@owtoph.com>
2018-11-17 10:08:46 +0800

    di ko padin mapagana tangina

--- server/server.js
+++ server/server.js
@@ -2,8 +2,15 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var path = require('path');
 
 var app = module.exports = loopback();
+app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
+app.set('json spaces', 2); // format json responses for easier viewing
+
+// must be set to serve views properly when starting the app via `slc run` from
+// the project root
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.start = function() {
   // start the web server
